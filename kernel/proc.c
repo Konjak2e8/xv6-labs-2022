ff6b36b054112709ab655b26f9dfaa2b255b3878
Konjak2e8 <734977762@qq.com>
2023-08-05 20:53:07 +0800

    feat: add calculation of freemem and proc_num

--- kernel/proc.c
+++ kernel/proc.c
@@ -683,3 +683,19 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64
+nproc(void)
+{
+  uint64 counter = 0;
+  struct proc *p;
+
+  for(p = proc; p < &proc[NPROC]; p++) {
+    acquire(&p->lock);
+    if(p->state != UNUSED) {
+      ++counter;
+    }
+    release(&p->lock);
+  }
+  return counter;
+}
