d46f1f4f6d0ded06f87f4ccdc52799e327cbb0aa
Konjak2e8 <734977762@qq.com>
2023-08-09 15:03:40 +0800

    feat: define cowhandler()

--- kernel/trap.c
+++ kernel/trap.c
@@ -29,6 +29,32 @@ trapinithart(void)
   w_stvec((uint64)kernelvec);
 }
 
+// check the status the required page to decide how to alloc mem
+int
+cowhandler(pagetable_t pagetable, uint64 va)
+{
+    char *mem;
+    if (va >= MAXVA)
+      return -1;
+    pte_t *pte = walk(pagetable, va, 0);
+    if (pte == 0)
+      return -1;
+    // check the PTE
+    if ((*pte & PTE_RSW) == 0 || (*pte & PTE_U) == 0 || (*pte & PTE_V) == 0) {
+      return -1;
+    }
+    if ((mem = kalloc()) == 0) {
+      return -1;
+    }
+    uint64 pa = PTE2PA(*pte); // old physical address
+    memmove((char*)mem, (char*)pa, PGSIZE); // copy old data to new mem
+    kfree((void*)pa);
+    uint flags = PTE_FLAGS(*pte);
+    *pte = (PA2PTE(mem) | flags | PTE_W); // set PTE_W to 1, change the address pointed to by PTE to new mempage
+    *pte &= ~PTE_RSW; // set PTE_RSW to 0
+    return 0;
+}
+
 //
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
@@ -65,6 +91,14 @@ usertrap(void)
     intr_on();
 
     syscall();
+  }
+  else if (r_scause() == 13 || r_scause() == 15) { // write page fault
+    uint64 va = r_stval();
+    if (va >= p->sz)
+      p->killed = 1;
+    int ret = cowhandler(p->pagetable, va);
+    if (ret != 0)
+      p->killed = 1;
   } else if((which_dev = devintr()) != 0){
     // ok
   } else {
